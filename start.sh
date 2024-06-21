3b8455aa9bc187e2ac56e754c73ebcb6c186103c
Ziyi.Gao <15221024927@163.com>
2024-06-21 12:05:36 +0800

    start.sh add chmod u+x main
    Signed-off-by: Ziyi.Gao <15221024927@163.com>

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # qt-plugins
 export QT_QPA_PLATFORM_PLUGIN_PATH=/home/co2m/mambaforge/envs/opencv/lib/qt6/plugins/platforms
-
+chmod u+x ./main_new
 # start executable
 while true; do
     ./main_new
